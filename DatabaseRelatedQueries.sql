e23ca66f36189978fabe51028dc93167a8acd6b9
Sonam Narula <sonamnarula2108@gmail.com>
2025-04-13 18:21:27 +0530

    Add files via upload

---
+++ DatabaseRelatedQueries.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+
+CREATE TABLE student (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    age INT NOT NULL
+);
+
+INSERT INTO student VALUES (1, "SONAM", 19);
+INSERT INTO student VALUES (2, "ARPIT", 21);
+
+SELECT * FROM student;
+
+SHOW DATABASES;
+SHOW TABLES;
