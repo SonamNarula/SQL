c0677160fd63bb4d06a7da43b0e213ae9b024ad5
Sonam Narula <sonamnarula2108@gmail.com>
2025-04-13 19:13:43 +0530

    Add files via upload

---
+++ Practice Question - 1.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS xyz_company;
+USE xyz_company;
+
+CREATE TABLE employee(
+	id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT
+);
+
+INSERT INTO employee 
+(id, name , salary)
+VALUES
+(1, "adam", 25000),
+(2, "bob", 30000),
+(3, "casey", 40000);
+
+SELECT * FROM employee;
