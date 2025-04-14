0aacb58145b4730f6cb3ea3bc119a36cc7ebb2e3
Sonam Narula <sonamnarula2108@gmail.com>
2025-04-14 09:31:21 +0530

    Add files via upload

---
+++ Operators.sql
@@ -0,0 +1,50 @@
+CREATE DATABASE IF NOT EXISTS college;
+USE college;
+
+CREATE TABLE students(
+	Rollno INT PRIMARY KEY, 
+    name VARCHAR(50),
+    marks INT NOT NULL,
+    grade VARCHAR(1),
+    city VARCHAR(20)
+);
+
+SELECT * FROM students;
+
+INSERT INTO students
+(RollNo, name, marks, grade, city)
+VALUES
+(101, "anil", 78, "C", "Pune"),
+(102, "bhumika", 93, "A", "Mumbai"),
+(103, "chetan", 85, "B", "Mumbai"),
+(104, "dhruv", 96, "A", "Delhi"),
+(105, "emanuel", 12, "F", "Delhi"),
+(106, "farah", 82, "B", "Delhi");
+
+SELECT DISTINCT city FROM students;
+
+SELECT * 
+FROM students
+WHERE marks + 10 > 100 AND city = "Mumbai";
+
+
+SELECT * 
+FROM students
+WHERE marks + 10 > 100 OR city = "Mumbai";
+
+
+
+SELECT * 
+FROM students
+WHERE marks BETWEEN 80 AND 100;
+
+
+
+SELECT * 
+FROM students
+WHERE city IN ("Mumbai", "Delhi");
+
+
+SELECT * 
+FROM students
+WHERE city NOT IN ("Mumbai", "Delhi");
